5b807b904ab4d34ac52bde0f4e979cc372559872
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-13 16:08:56 +0100

    Updates  :rocket:

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import "./LoginSignup.css"
 
+import email from "../Components/Assets/email.png"
+import user from "../Components/Assets/person.png"
+import password from "../Components/Assets/password.png"
+
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>Sign Up</div>
+            <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+            <div className='input'>
+                <img src={user} alt=''/>
+                <input type='text'/>
+            </div>
+            <div className='input'>
+                <img src={email} alt=''/>
+                <input type='email'/>
+            </div>
+            <div className='input'>
+                <img src={password} alt=''/>
+                <input type='password'/>
+            </div>
+        </div>
+        <div className='forgot-password'> Lost Password? <span>Click Here!</span></div>
+        <div className='submit-container'>
+            <div className='submit'>Sign Up</div>
+            <div className='submit'>Login</div>
+        </div>
+    </div>
   )
 }
